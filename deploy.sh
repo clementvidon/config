5327683afb184b2c9a16e3dceb0c88d3c47cdcdc
Clément VIDON <cvidon@student.42.fr>
2022-08-23 07:56:17 +0200

    Add i3 conf

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,8 @@
 
 mkdir -pv $HOME/.fonts
 ln -fsv $HOME/git/config/.fonts/*.ttc                   $HOME/.fonts/
-
-if command -v vim &> /dev/null; then
-    mkdir -pv $HOME/.config/vim/autoload
-    mkdir -pv $HOME/.config/vim/autoload/plugin
-    mkdir -pv $HOME/.config/vim/after/ftplugin
-    mkdir -pv $HOME/.config/vim/syntax
-    ln -fsv $HOME/git/config/.vimrc                     $HOME/.vimrc
-    ln -fsv $HOME/git/config/vim/*.vim                  $HOME/.config/vim/
-    ln -fsv $HOME/git/config/vim/autoload/plug.vim      $HOME/.config/vim/autoload/plug.vim
-    ln -fsv $HOME/git/config/vim/autoload/plugin/*.vim  $HOME/.config/vim/autoload/plugin/
-    ln -fsv $HOME/git/config/vim/after/ftplugin/*.vim   $HOME/.config/vim/after/ftplugin/
-    ln -fsv $HOME/git/config/vim/syntax/*.vim           $HOME/.config/vim/syntax/
-fi
+mkdir -pv $HOME/.icons
+ln -fsv $HOME/git/config/.icons/*                       $HOME/.icons/
 
 if command -v git &> /dev/null; then
     ln -fsv $HOME/git/config/.gitconfig                 $HOME/.gitconfig
@@ -39,4 +28,20 @@ fi
 if command -v i3 &> /dev/null; then
     mkdir -pv $HOME/.config/i3/
     ln -fsv $HOME/git/config/i3/config                  $HOME/.config/i3/
+    ln -fsv $HOME/git/config/.xinitrc                   $HOME/.xinitrc
+    ln -fsv $HOME/git/config/.Xresources                $HOME/.Xresources
+fi
+
+if command -v vim &> /dev/null; then
+    mkdir -pv $HOME/.config/vim/autoload
+    mkdir -pv $HOME/.config/vim/autoload/plugin
+    mkdir -pv $HOME/.config/vim/after/ftplugin
+    mkdir -pv $HOME/.config/vim/syntax
+    ln -fsv $HOME/git/config/.vimrc                     $HOME/.vimrc
+    ln -fsv $HOME/git/config/vim/*.vim                  $HOME/.config/vim/
+    ln -fsv $HOME/git/config/vim/autoload/plug.vim      $HOME/.config/vim/autoload/plug.vim
+    ln -fsv $HOME/git/config/vim/autoload/plugin/*.vim  $HOME/.config/vim/autoload/plugin/
+    ln -fsv $HOME/git/config/vim/after/ftplugin/*.vim   $HOME/.config/vim/after/ftplugin/
+    ln -fsv $HOME/git/config/vim/syntax/*.vim           $HOME/.config/vim/syntax/
+    vim -c ':PlugInstall'
 fi
